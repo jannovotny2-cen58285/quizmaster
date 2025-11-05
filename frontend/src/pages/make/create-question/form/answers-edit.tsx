29b728fd2abca38ec25ca47316e9d57c0aeec2fb
Karel Smutny <karel.smutny@outlook.com>
2025-11-05 08:32:08 +0100

    refactor: Used standard components in answer edit

--- frontend/src/pages/make/create-question/form/answers-edit.tsx
+++ frontend/src/pages/make/create-question/form/answers-edit.tsx
@@ -1,3 +1,4 @@
+import { Button, TextInput } from 'pages/components'
 import type { AnswerState } from './question-form-state.ts'
 
 interface AnswerRowProps {
@@ -12,41 +13,17 @@ export const AnswerRow = ({ state, isMultipleChoice }: AnswerRowProps) => (
                 className={!isMultipleChoice ? 'answer-isCorrect-checkbox' : 'answer-isCorrect-checkbox-multi'}
                 type="checkbox"
                 checked={state.isCorrect}
-                onChange={() => state.toggleCorrect()}
+                onChange={state.toggleCorrect}
             />
             <span className="answer-row-correct-icon">{state.isCorrect ? '✅' : '❌'}</span>
             <span className="answer-row-correct-text">{state.isCorrect ? 'Correct answer' : 'Incorrect answer'}</span>
         </div>
         <div className="answer-row-section">
-            <input
-                className="text"
-                type="text"
-                placeholder={`Input answer ${state.index + 1} here...`}
-                value={state.answer}
-                onChange={e => state.setAnswer(e.target.value)}
-            />
+            <TextInput className="text" value={state.answer} onChange={state.setAnswer} />
         </div>
         <div className="answer-row-section">
-            <input
-                className="explanation"
-                type="text"
-                placeholder="You can add explanation of the anwser here..."
-                value={state.explanation}
-                onChange={e => state.setExplanation(e.target.value)}
-            />
+            <TextInput className="explanation" value={state.explanation} onChange={state.setExplanation} />
         </div>
-    </div>
-)
-
-interface AddAnswerProps {
-    readonly addAnswer: () => void
-}
-
-export const AddAnswerButton = ({ addAnswer }: AddAnswerProps) => (
-    <div>
-        <button type="button" onClick={addAnswer} className="secondary button" id="add-answer">
-            Add Answer
-        </button>
     </div>
 )
 
@@ -63,7 +40,11 @@ export const AnswersEdit = ({ answerStates, isMultipleChoice, addAnswer }: Answe
             {answerStates.map(state => (
                 <AnswerRow state={state} isMultipleChoice={isMultipleChoice} />
             ))}
-            <AddAnswerButton addAnswer={addAnswer} />
+            <div>
+                <Button onClick={addAnswer} className="secondary button" id="add-answer">
+                    Add Answer
+                </Button>
+            </div>
         </>
     )
 }
